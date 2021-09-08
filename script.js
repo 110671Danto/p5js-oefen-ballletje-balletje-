b02be11a9e2336d12f58646ad89e7d84af6e01ee
Danto Rosier <74553165+110671Danto@users.noreply.github.com>
2021-09-08 12:13:29 +0000

    ball beweegt op en neer van links naar rechts

--- script.js
+++ script.js
@@ -3,26 +3,34 @@ var x, y, v, h;
 function setup() {
 	createCanvas(500, 400);
 
+
   x = 50;
   y = 50;
   v = 6;
   h = 2;
+
+  
 }
 
 function draw() {
 	background(225);
   
-  ellipse(x,y,50,50);
+  ellipse(x,y,50,50); {
   x = x + v;
   y = y + h;
+  }
 
-
-
-  if(x <= 20 || x >= 480) {
+ 
+  if(x <= 24 || x >= 476) {
     v = v * -1;
   }
-  if(y <= 20 || y >= 380) {
+  if(y <= 24 || y >= 376) {
     h = h * -1;
   }
+
+
 }
+
+
+
 
